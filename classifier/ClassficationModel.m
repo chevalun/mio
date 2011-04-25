6be116b4f8ab3ac6b56c85f8450b1a7c1ecd1b9d
Zifei TONG <soariez@gmail.com>
2011-04-25 21:49:40 +0800

    finalize interface and kNNModel

--- classifier/ClassficationModel.m
+++ classifier/ClassficationModel.m
@@ -1,24 +1,33 @@
 classdef ClassficationModel
     % Interface for a ClassficationModel
     
-   properties (Access = protected)
-      fea
-      gnd
-      n
-      m
-   end
+    properties (Access = protected)
+        fea  % features
+        gnd  % ground truth
+        n    % number of training samples
+        m    % number of dimensions of feature
+    end
     
-    methods (Abstract)
-        cls = predict(sample);
+    methods (Abstract, Access = protected)
+        cls = predict_one(sample);  % predict classes for a given test sample
     end
     
     methods (Access = protected)
-        function obj = ClassficationModel(fea, gnd)
+        function obj = ClassficationModel(fea, gnd)  % initialize features and ground truth
             obj.fea = fea;
             obj.gnd = gnd;
             [obj.n, obj.m] = size(fea);
         end
     end
     
+    methods
+        function cls = predict(obj, samples)  % predict classes for given test samples
+            [nsamples, ~] = size(samples);
+            cls = zeros(nsamples, 1);
+            for col = 1:nsamples
+                cls(col) = predict_one(obj, samples(col,:));
+            end
+        end
+    end
 end
 
--- classifier/kNNModel.m
+++ classifier/kNNModel.m
@@ -1,30 +1,30 @@
 classdef kNNModel < ClassficationModel
-    % k-nearest neighbor algorithm
-    
+    % k-nearest neighbor classifier
+
     properties (Access = private)
-        k         % the kNN parameter
+        k  % the kNN parameter, k
     end
     
     methods
-        function obj = kNNModel(fea, gnd, options)
+        function obj = kNNModel(fea, gnd, options)  % initialize kNN model
             obj = obj@ClassficationModel(fea, gnd);
             obj.k = options.k;
         end
         
-        function cls = predict(obj, samples)
-            [nsamples, ~] = size(samples);
-            cls = zeros(nsamples, 1);
-            for col = 1:nsamples 
-                dis = sum((repmat(samples(col,:), obj.n, 1) -  obj.fea).^2, 2);  % compute distances
-                [~, ix] = sort(dis);
-                s = zeros(obj.m,1);
-                for i = 1:obj.k   % find k points with shortest distance to vote for the predicted class
-                    s(obj.gnd(ix(i))) = s(obj.gnd(ix(i))) + 1;
-                end
-                [~, cls(col)] = max(s);
-            end
+        function cls = predict(obj, samples)  % predict classes for given test samples
+            cls = predict@ClassficationModel(obj, samples);
         end
     end
     
+    methods (Access = protected)
+        function cls = predict_one(obj, sample)
+            dis = sum((repmat(sample, obj.n, 1) -  obj.fea).^2, 2);  % compute distances
+            [~, ix] = sort(dis);
+            s = zeros(obj.n, 1);
+            for i = 1:obj.k   % find k points with shortest distance to vote for the predicted class
+                s(obj.gnd(ix(i))) = s(obj.gnd(ix(i))) + 1;
+            end
+            [~, cls] = max(s);
+        end
+    end
 end
-
